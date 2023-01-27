2066fcad60ed3b1f32c5a5a291a57784c72f3d02
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-01-27 17:11:41 +0530

    fourth commit update

--- tetwo.java
+++ tetwo.java
@@ -1,3 +1,13 @@
+/* echo "# Yash-DS-n-Algo-2023" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Yash-Sisodia/Yash-DS-n-Algo-2023.git
+git push -u origin main */
+
+
+
 import java.util.*;
 public class tetwo {
     public static void main(String[] args) {
