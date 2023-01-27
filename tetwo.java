0d8451c04005128117f23d58f91d2a58903c3066
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-01-27 17:09:45 +0530

    fourth test commit

---
+++ tetwo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class tetwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n + 2;
+        System.out.println(a);
+    }
+}
