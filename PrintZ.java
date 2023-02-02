eeea29406276299fde713879f3c239415db44eb0
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-02-02 21:52:35 +0530

    Day-1 Print Z

---
+++ PrintZ.java
@@ -0,0 +1,29 @@
+/*
+Question 1 - Print Z
+
+1. You are required to print a 'z' of size 5 using '*'.
+Input Format
+There is no input
+
+Output Format
+print-z-in-java
+
+*****
+   *
+  *
+ *
+*****
+
+ */
+
+
+import java.util.*;
+public class PrintZ {
+    public static void main(String[] args) {
+       System.out.println("*****");
+       System.out.println("   *");
+       System.out.println("  *");
+       System.out.println(" *");
+       System.out.println("*****");
+    }
+}
