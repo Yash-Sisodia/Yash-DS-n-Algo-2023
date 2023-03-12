59a308526350dd3b45bbbae153cd08f5b74fc4b9
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-03-12 12:51:48 +0530

    Hello Worldgit add hello.java with a lot of procrastination again i am starting my coding journey

---
+++ GettingStartedwithjava/hello.java
@@ -0,0 +1,17 @@
+package GettingStartedwithjava;
+
+
+/* echo "# Yash-DS-n-Algo-2023" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Yash-Sisodia/Yash-DS-n-Algo-2023.git
+git push -u origin main */
+
+import java.util.*;
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
