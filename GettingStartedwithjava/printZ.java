10361664eab1ad9a7485c8b03e7e930ad8f1a99f
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-03-12 13:04:26 +0530

    Ques 1

---
+++ GettingStartedwithjava/printZ.java
@@ -0,0 +1,44 @@
+package GettingStartedwithjava;
+
+
+// Date : 12-03-2023 
+/* Question 1 -> Print Z
+
+Input Format
+There is no input
+
+Output Format
+print-z-in-java
+*****
+   *
+  *
+ *
+*****
+
+Constraints
+No Constraints
+Sample Output
+*****
+   *
+  *
+ *
+*****
+
+ */
+
+
+
+
+import java.util.*;
+public class printZ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+        
+    }
+}
