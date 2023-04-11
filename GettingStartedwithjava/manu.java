6d1bf70aed599f4f49ccb8075e885bb883e46930
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-04-11 06:03:05 +0530

    Manu file

---
+++ GettingStartedwithjava/manu.java
@@ -0,0 +1,18 @@
+package GettingStartedwithjava;
+
+import java.util.*;
+public class manu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n==1) {
+            System.out.println("Hello Manu!!");
+        } 
+        else {
+            System.out.println("I am not Manu");
+        }
+
+    }
+    
+}
