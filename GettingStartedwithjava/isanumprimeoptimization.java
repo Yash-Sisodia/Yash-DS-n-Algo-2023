bd6a46b1721144a07c79b79d11f7b569a7aa3fcd
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-04-13 05:41:30 +0530

    Prime number program optimization

---
+++ GettingStartedwithjava/isanumprimeoptimization.java
@@ -0,0 +1,27 @@
+package GettingStartedwithjava;
+
+
+import java.util.*;
+public class isanumprimeoptimization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // number of times your loop is going to run
+
+        for(int i = 1 ; i <=t ; i++) {
+            int n = sc.nextInt();
+            int counter = 0;
+
+            for(int j = 2 ; j*j<=n ; j++) {
+                if(n%j==0) {
+                    counter++;
+                }
+            }
+            if(counter==0) {
+                System.out.println("prime");
+            } 
+            else {
+                System.out.println("not prime");
+            }
+        }
+    }
+}
---
+++ GettingStartedwithjava/printprimebtwrange.java
@@ -0,0 +1,24 @@
+package GettingStartedwithjava;
+
+
+import java.util.*;
+public class printprimebtwrange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+
+        for(int i = low ; i<high ; i++) {
+            int counter = 0;
+            for(int j = 2 ; j<low; j++) {
+                if(i%j==0) {
+                    counter++;
+                }
+            }
+            if(counter==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
