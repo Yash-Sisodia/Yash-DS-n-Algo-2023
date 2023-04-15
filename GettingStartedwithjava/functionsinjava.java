ac99122c45b2e17ff8be82050c27731931f045ca
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2023-04-15 07:56:28 +0530

    Functions in Java

---
+++ GettingStartedwithjava/functionsinjava.java
@@ -0,0 +1,17 @@
+package GettingStartedwithjava;
+
+
+import java.util.*;
+public class functionsinjava {
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        printMyName(name); // Call krna function ko ....
+    }
+}
